404a005a3a5657500b5d61914ac731167006cd98
Elzair <elzairthesorcerer@gmail.com>
2022-05-13 12:56:36 -0400

    Added EntrySkill to MT2E

--- src/EntrySkill.js
+++ src/EntrySkill.js
@@ -20,6 +20,17 @@ export function EntrySkill({ game, career, upp, skills, updateSkills, display, o
                 updateLog={updateLog}
             />
         );
+    } else if (display && game === 'mt2e') {
+        return (
+            <EntrySkillMT2E
+                career={career}
+                upp={upp}
+                skills={skills}
+                updateSkills={updateSkills}
+                onSkillSelection={onSkillSelection}
+                updateLog={updateLog}
+            />
+        );
     } else {
         return (<div></div>)
     }
@@ -73,6 +84,75 @@ function EntrySkillCE({ career, upp, skills, updateSkills, onSkillSelection, upd
     });
 
     if (career.length === 1) {
+        return (<div></div>);
+    } else {
+        // Allow traveller to learn a career's service skill at level 0 if they do not already know it.
+        const careerData = CECAREERS.filter(c => c.name === career[career.length - 1].branch)[0];
+        let skillsDisplay = careerData.sst.filter(s => !skills.hasOwnProperty(s.name))
+            .map(s => <div key={`skill-${s.name}-div`} className="CESkill">
+                <input type="radio" id={`skill-${s.name}`} name="skill" value={s.name} /> <label className="CESkillLabel" htmlFor={`skill-${s.name}`} >{capitalize(s.name)}</label>
+            </div>);
+
+        return (
+            <form className="CECAREERS" onSubmit={selectSkill}>
+                <p>Select One Skill: </p>
+                {skillsDisplay}
+                <input className="Submit" type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+// .##.....##.########..#######..########
+// .###...###....##....##.....##.##......
+// .####.####....##...........##.##......
+// .##.###.##....##.....#######..######..
+// .##.....##....##....##........##......
+// .##.....##....##....##........##......
+// .##.....##....##....#########.########
+
+function EntrySkillMT2E({ career, upp, skills, updateSkills, onSkillSelection, updateLog }) {
+    function selectSkill(ev) {
+        ev.preventDefault();
+
+        let skillName = '';
+        for (let s of ev.target) {
+            if (s.checked) {
+                skillName = s.value;
+            }
+        }
+
+        updateSkills({ [skillName]: 0 });
+        updateLog(`You gain ${skillName}-0.`);
+        onSkillSelection();
+    }
+
+    // If this is the traveller's first career or they enrolled in military school, give them all service skills at level 0.
+    // If neither of those things is true, then select one skill from the career's service skill table to be level 0.
+    useEffect(() => {
+        let careerName = career[career.length-1].branch;
+        if (career.length === 1 || careerName === 'army-academy' || careerName === 'naval-academy' || careerName) {
+            const careerData = CECAREERS.filter(c => c.name === career[career.length - 1].branch)[0];
+            const serviceSkills = careerData.sst.filter(skill => skill.type === 'SKILL').map(skill => skill.name);
+            let newLog = [];
+
+            let newSkills = {};
+            serviceSkills.forEach(skill => {
+                // Only give the traveller a skill they do not already have
+                if (!skills.hasOwnProperty(skill)) {
+                    newSkills[skill] = 0;
+                    newLog.push(`You gain ${skill}-0.`);
+                }
+            });
+
+            updateSkills(newSkills);
+            updateLog(newLog);
+            onSkillSelection();
+        }
+    });
+
+    let careerName = career[career.length-1].branch;
+    if (career.length === 1 || careerName === 'army-academy' || careerName === 'naval-academy' || careerName) {
         return (<div></div>);
     } else {
         // Allow traveller to learn a career's service skill at level 0 if they do not already know it.
